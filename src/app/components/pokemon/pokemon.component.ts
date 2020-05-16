b46ead249f2873bb00e935ee6b03a7d48809ac73
DOligex <olinours@yahoo.fr>
2020-05-16 16:55:25 +0200

    build: pokemon

---
+++ src/app/components/pokemon/pokemon.component.ts
@@ -0,0 +1,23 @@
+import { POKEMONS } from './../../../shared/mock-pokemon';
+import { Component, OnInit } from '@angular/core';
+
+import { Pokemon } from '../../../shared/pokemon';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.scss']
+})
+export class PokemonComponent implements OnInit {
+
+  constructor() { }
+
+	private pokemons: Pokemon[] = null;
+
+	ngOnInit() {
+		this.pokemons = POKEMONS;
+	}
+	selectPokemon(pokemon: Pokemon) {
+		alert('Vous avez cliqué sur ' + pokemon.name);
+	}
+}
