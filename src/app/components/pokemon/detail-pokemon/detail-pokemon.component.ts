d97f594c5a82b734ba8fb4724eb7f2276eab0f98
DOligex <olinours@yahoo.fr>
2020-06-10 11:48:11 +0200

    build: detail pokemon compo

---
+++ src/app/components/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../../../shared/class/pokemon';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { PokemonsService } from '../../../shared/services/pokemons.service';
+
+@Component({
+  selector: 'app-detail-pokemon',
+  templateUrl: './detail-pokemon.component.html',
+  styleUrls: ['./detail-pokemon.component.scss']
+})
+export class DetailPokemonComponent implements OnInit {
+  pokemon: Pokemon = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private pokemonsService: PokemonsService
+  ) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.pokemonsService.getPokemon(id).subscribe(pokemon => this.pokemon = pokemon);
+  }
+
+  goBack(): void {
+    this.router.navigate(['/pokemons']);
+  }
+
+  goEdit(pokemon: Pokemon): void {
+  const link = ['/pokemon/edit', pokemon.id];
+  this.router.navigate(link);
+  }
+
+  delete(pokemon: Pokemon): void {
+    this.pokemonsService.deletePokemon(pokemon)
+    .subscribe(_ => this.goBack());
+  }
+}
