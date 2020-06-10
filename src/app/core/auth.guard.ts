f95e384a7eda01c7348ed020e840051aca0f6a34
DOligex <olinours@yahoo.fr>
2020-06-10 11:53:26 +0200

    build: auth

---
+++ src/app/core/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.authService.isLoggedIn) { return true; }
+    this.authService.redirectUrl = url;
+    this.router.navigate(['/login']);
+
+    return false;
+  }
+
+    // méthode d'origine générée par le ng g guard
+      // canActivate(
+      //   next: ActivatedRouteSnapshot,
+      //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      //   return true;
+      // }
+}
