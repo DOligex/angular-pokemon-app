5cb3f89b80360997798672fb4a2fad052a5265d3
DOligex <olinours@yahoo.fr>
2020-06-10 11:49:17 +0200

    build: pokemon navbar compo

---
+++ src/app/components/pokemon-navbar/pokemon-navbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PokemonNavbarComponent } from './pokemon-navbar.component';
+
+describe('PokemonNavbarComponent', () => {
+  let component: PokemonNavbarComponent;
+  let fixture: ComponentFixture<PokemonNavbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PokemonNavbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PokemonNavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/pokemon-navbar/pokemon-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-navbar',
+  templateUrl: './pokemon-navbar.component.html',
+  styleUrls: ['./pokemon-navbar.component.scss']
+})
+export class PokemonNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
